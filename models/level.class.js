b9920a019c80a2ec47a0fd91ec8ce8a388848113
Fabian Gretsch <91975420+fab0802@users.noreply.github.com>
2023-02-22 20:34:57 +0100

    added level class

---
+++ models/level.class.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Level {
+  enemies;
+  clouds;
+  backgroundObjects;
+
+  constructor(enemies, clouds, backgroundObjects) {
+    this.enemies = enemies;
+    this.clouds = clouds;
+    this.backgroundObjects = backgroundObjects;
+  }
+}
